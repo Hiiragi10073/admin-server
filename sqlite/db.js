fe696e8d8a8bc287cb7b1ac9e153a5ae83a8108f
Hiiragi <yuki100073@gmail.com>
2021-10-02 13:56:53 +0800

    fix: 数据库

--- sqlite/db.js
+++ sqlite/db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-
 let sqliteDbPath = process.env.DB_PATH
+const db = new sqlite3.Database(sqliteDbPath)
 
-module.exports.db = new sqlite3.Database(sqliteDbPath)
+module.exports = db
